f4ce292bc14791f3068799321dbdd0d51528f356
Vasanth Kumar Thota - STUDENT <l00186073@atu.ie>
2024-05-09 11:16:48 +0100

    Commit

--- Pratice.R
+++ Pratice.R
@@ -39,4 +39,46 @@ attach(cars)
 linearmod<-lm(dist~speed)
 linearmod
 
+no_of_records<-sample(1:nrow(cars),0.8 * nrow(cars))
+str(no_of_records)
 
+training_data<-cars[no_of_records,]
+training_data
+
+testing_data<-cars[-no_of_records,]
+testing_data
+
+lr_model<-lm(dist~speed, data=training_data)
+lr_model
+summary(lr_model)
+
+dist_predicted<-predict(lr_model, testing_data) 
+dist_predicted
+
+actual_preds<-data.frame(cbind(actuals = testing_data$dist, predicted = dist_predicted))
+actual_preds
+
+head(actual_preds)
+
+attach(actual_preds)
+correlation_accuracy<- cor(actuals, predicted)
+correlation_accuracy
+
+correlation_accuracy1<-cor(actual_preds)
+correlation_accuracy1
+
+min_max_accuracy<- mean(apply(actual_preds,1,min)/ apply(actual_preds,1,max))
+min_max_accuracy
+
+mape<-mean(abs(predicted - actuals)/ actuals)
+mape
+
+
+#install.packages("DAAG")
+library(DAAG)
+
+windows(20,12)
+cvResults<-suppressWarnings(CVlm(data = cars, form.lm = dist~speed, m=5, dots =FALSE,
+                                 seed = 29, legend.pos = "topleft", printit = FALSE,
+                                 main = "Small Symbols are predicted vales while bigger ones are actuals"))
+cvResults
